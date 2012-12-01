aaf82732288f39145dec45f829c8ca54a4319dc1
Gianni Chiappetta <gianni@runlevel6.org>
2012-11-30 22:54:36 -0800

    Remove remainder of md2html junk

--- lib/markdown_page.rb
+++ lib/markdown_page.rb
@@ -1,8 +1,25 @@
-require 'erector'
 require 'doc_page'
+require 'erector'
+require 'markdown_renderer'
+require 'redcarpet'
 
 class MarkdownPage < DocPage
   def doc_content
-    rawtext md2html(src)
+    rawtext markdown.render(src)
+  end
+
+  protected
+
+  def markdown
+    @@markdown ||= Redcarpet::Markdown.new(
+      MarkdownRenderer,
+      :autolink           => true,
+      :fenced_code_blocks => true,
+      :lax_spacing        => true,
+      :no_intra_emphasis  => true,
+      :strikethrough      => true,
+      :superscript        => true,
+      :tables             => true
+    )
   end
 end
